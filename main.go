2c0733758c9e52a1507f3dba0190eee9340f3a06
Businge931 <jimmybusinge9@gmail.com>
2024-06-07 09:24:13 +0300

    14. Interfaces To The Rescue

--- main.go
+++ main.go
@@ -1,19 +1,21 @@
 package main
 
 import (
-	"fmt"
+	// "fmt"
 
 	"example.com/pricecalculator/cmdmanager"
-	"example.com/pricecalculator/filemanager"
+	// "example.com/pricecalculator/filemanager"
 	"example.com/pricecalculator/prices"
 )
+
+
 
 func main() {
 	taxRates := []float64{0, 0.07, 0.1, 0.15}
 
 	for _, taxRate := range taxRates {
-		fm := filemanager.New("prices.txt", fmt.Sprintf("result_%.0f.json", taxRate*100))
-		cmdm :=cmdmanager.New()
+		// fm := filemanager.New("prices.txt", fmt.Sprintf("result_%.0f.json", taxRate*100))
+		cmdm := cmdmanager.New()
 		priceJob := prices.NewTaxIncludedPriceJob(cmdm, taxRate)
 		priceJob.Process()
 	}
